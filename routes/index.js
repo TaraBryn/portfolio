8878aa17fc83804af827e01d103bdf95e8b0e9fb
Tara Stahler <tbstahler@gmail.com>
2019-12-09 17:12:26 -0500

    broke up stylesheets, set up projects template and links to the template

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', front_end });
 });
 
-router.get('/projects/front-end', (req, res) => {
-  let {front_end} = require(global.root + '/public/json/projects.json');
-  res.json(front_end)
+router.get('/projects/frontend/:project', (req, res) => {
+  res.render('front-end', require(global.root + '/public/json/projects.json').front_end[req.params.project]);
 })
 
 router.get('/test', (req, res)=>res.send('successful'))
